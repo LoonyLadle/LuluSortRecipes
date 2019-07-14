454502986e0750797563c6f036bf3cf43c9ef8f5
LoonyLadle <LoonyLadle@users.noreply.github.com>
2019-07-14 16:03:11 -0400

    1.0.0

---
+++ README.md
@@ -0,0 +1,7 @@
+# Lulu's Sort Recipes
+Sorts the recipe def database.
+
+**_This mod MUST be loaded last!_**
+
+## What does that even mean?
+Sorting the recipe def database will cause recipes to appear in alphabetical order on workbenches, surgery bill menu, and anywhere else recipes are referenced. Some recipes remain unsorted for reasons that currently elude me, but the situation should still be vastly improved from what it was before.
---
+++ Source/LuluSortRecipes/SortRecipes.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Verse;
+
+#pragma warning disable IDE1006 // Naming Styles
+
+namespace LoonyLadle.SortRecipes
+{
+    [StaticConstructorOnStartup]
+    public static class SortRecipes
+    {
+        static SortRecipes()
+        {
+            List<RecipeDef> sortedList = DefDatabase<RecipeDef>.AllDefs.OrderBy(d => d.label).ToList();
+            typeof(DefDatabase<RecipeDef>).GetField("defsList", BindingFlags.NonPublic | BindingFlags.Static).SetValue(new object(), sortedList);
+            Log.Message("[Lulu's Sort Recipes] The def database has been sorted.");
+        }
+    }
+}
